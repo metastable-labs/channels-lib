d2a0e94c881b83f7bd60a7dea6a93efc24e4f406
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-06-28 09:58:40 -0600

    minor updates on test and versioning

--- test/channels.test.ts
+++ test/channels.test.ts
@@ -1,23 +1,21 @@
 import { config } from 'dotenv';
-import Launchbox from "../src";
+import Launchbox from '../src';
 
 config();
 
 describe('Launchbox SDK Test', () => {
-  let launchbox = new Launchbox(process.env.TEST_AIRSTACK_KEYS!, "dev");
+  let launchbox = new Launchbox(process.env.TEST_AIRSTACK_KEYS!, 'dev');
   let owner = '0x459D7FB72ac3dFB0666227B30F25A424A5583E9c';
-
 
   it('should get channel data by creators', async () => {
     const response = await launchbox.getChannelsByUserAddress(owner as `0x${string}`);
     expect(response.FarcasterChannels).toBeDefined();
-    expect(response.FarcasterChannels.FarcasterChannel.length).toBeGreaterThanOrEqual(1)
+    expect(response.FarcasterChannels.FarcasterChannel.length).toBeGreaterThanOrEqual(1);
   }, 200000);
 
   it('should get limited channel data by creator', async () => {
     const response = await launchbox.getChannelsByUserAddress(owner as `0x${string}`, 1);
     expect(response.FarcasterChannels).toBeDefined();
-    expect(response.FarcasterChannels.FarcasterChannel.length).toBe(1)
+    expect(response.FarcasterChannels.FarcasterChannel.length).toBe(1);
   }, 200000);
-
 });
